9ac3bdf23763b3e926ec4c55a80ca40891876cb5
zhaoyilong <zhaoyilong@vm20105.(none)>
2016-10-29 11:48:41 +0800

    fix install.sh & vimrc

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 echo "Make sure you have sudo priority, then see tmp/install.log ..."
-
+{
 set -x
-exec &>tmp/install.log
+mkdir -p tmp
+exec &>tmp/install.log &&
+dir=$(pwd)
 
-tar -zxf packages/cscope-15.8a.tar.gz -C tmp/
-tar -zxf packages/ctags-5.8.tar.gz -C tmp/
-cd tmp/ctags-5.8 && ./configure && make clean && make && sudo make install
-cd tmp/cscope-15.8a && ./configure && make clean && make && sudo make install
+tar -zxf ${dir}/packages/cscope-15.8a.tar.gz -C ${dir}/tmp/  &&
+tar -zxf ${dir}/packages/ctags-5.8.tar.gz -C ${dir}/tmp/ &&
+cd ${dir}/tmp/ctags-5.8 && ./configure && make clean && make && sudo make install &&
+pwd
+cd ${dir}/tmp/cscope-15.8a && ./configure && make clean && make && sudo make install &&
+pwd
 
 cd ../../
-touch ~/.vimrc && cat sources/vimrc  >~/.vimrc
+pwd
+touch ~/.vimrc && cat sources/vimrc  >~/.vimrc    &&
 mkdir -p ~/.vim && cp -rf sources/vim/* ~/.vim
+} &
 
+tail -f tmp/install.log
